b2b73c3d502c96b303e2a0cd915e368fbaaa359a
Blackett <FINIAN_BLACKETT@dpsk12.org>
2019-04-11 16:22:24 -0600

    Update bitwise_obfuscation.py

--- bitwise_obfuscation.py
+++ bitwise_obfuscation.py
@@ -1,4 +1,6 @@
 from z3 import *
+from arybo.lib import MBA, boolean_expr_solve
+import random
 
 BITS = 64
 
@@ -40,3 +42,26 @@ proofs = [
 assert(all([prove(proof) for proof in proofs]))
 
 print('Proved all equalities!')
+
+a = random.getrandbits(64)
+b = 0x7fffffffffffffff ^ a
+c = random.getrandbits(64)
+d = 0x7fffffffffffffff ^ b
+
+
+def f(X):
+  T = ((X+1)&(~X))
+  C = ((T | a) & c) + ((~T & b) | d)
+  return C
+
+m = MBA(64)
+c = m.var('x')
+print(f(c))
+
+0x7AFAFA697AFAFA69 0x80A061440A061440 0x10401050504 0x1010104
+
+"""
+sols = boolean_expr_solve(f(c)[63], c, 1)
+zero = sols[0].get_int_be()
+
+print(hex(zero))"""
